c55c241f9f0d54d610fb4617b5a165d10831a7c0
Cian Kaiser <85114729+Cian747@users.noreply.github.com>
2026-01-26 15:13:31 +0300

    Adding user roles for a custome user class

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser, PermissionsMixin):
+  class Role(models.TextChoices):
+      ADMIN, "Admin",
+      COACH, "Coach",
+      ATHLETE, "Athlete",
+
+  base_role = role.ATHLETE
+  role = models.CharField(max_length=50,choices=Role.choices, default=base_role)
